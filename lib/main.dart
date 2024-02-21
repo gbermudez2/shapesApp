7e91df3505b063dbf45a5032c14fe2a2957f83bb
gbermudez2 <gbermudezahs@gmail.com>
2024-02-20 17:27:40 -0800

    updated looks of title and main menu

--- lib/main.dart
+++ lib/main.dart
@@ -15,21 +15,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.lightBlueAccent,
         ),
@@ -43,16 +28,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -79,33 +54,23 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       backgroundColor: Colors.black87,
-      body: const Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: const Align(
+        alignment: Alignment.topCenter,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
             'Shapes',
-              style: TextStyle(fontSize: 50, color: Colors.white,
-              fontWeight: FontWeight.bold,),
+              style: TextStyle(fontSize: 70, color: Colors.white,
+              fontWeight: FontWeight.bold,
+                  letterSpacing: -2,
+                  fontFamily: 'Inter'),
             ),
             Text(
               'Learn to dance.',
-              style: TextStyle(fontSize: 20, color: Colors.white),
+              style: TextStyle(fontSize: 20, color: Colors.white,
+              letterSpacing: 6,
+                  fontFamily: 'Inter'),
             ),
           ],
         ),
@@ -121,7 +86,11 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
           child: const Padding(
             padding: EdgeInsets.symmetric(horizontal: 100, vertical: 20),
-            child: Text('Start'),
+            child: Text('BEGIN',
+            style: TextStyle(fontWeight: FontWeight.bold,
+                fontSize: 20,
+                letterSpacing: 10,
+                color: Colors.black87),),
           ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
--- lib/main_menu.dart
+++ lib/main_menu.dart
@@ -5,49 +5,29 @@ class MainMenu extends StatefulWidget {
 
   final String title;
 
+
   @override
   State<MainMenu> createState() => _MainMenuState();
 }
 
 class _MainMenuState extends State<MainMenu> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-      print("The counter has been increased to " + _counter.toString());
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-                'Main Menu'
-            ),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
-          ],
+    return const Scaffold(
+      backgroundColor: Colors.black87,
+      body: Align(
+        alignment: Alignment.topLeft,
+        child: Padding(
+          padding: EdgeInsets.only(left: 15, top: 80),
+          child: Text(
+            'Home',
+            style: TextStyle(fontSize: 50,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontFamily: 'Inter'),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
